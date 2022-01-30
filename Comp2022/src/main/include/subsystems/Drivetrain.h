715c9cc9a832723130fc6aec4e942c32405b502b
Reeva Randeri <rranderi.24@presentationhs.org>
2022-01-29 17:36:22 -0800

    Let limelight go to a certain range and shoot within that range
    -added a boolean m_endAtTarget that would only be true if the robot was within 3 to 10 feet of the hub
    -changed the numbers of the range in the header files
    -made an if/else to check if the robot was within the range

--- Comp2022/src/main/include/subsystems/Drivetrain.h
+++ Comp2022/src/main/include/subsystems/Drivetrain.h
@@ -226,7 +226,8 @@ public:
     void MoveWithJoysticks(frc::XboxController *driverPad);
     void MoveWithJoysticksEnd(void);
 
-    void MoveWithLimelightInit();
+    void MoveWithLimelightInit(bool m_endAtTarget);
+    double GetLimelightDistance(void);
     void MoveWithLimelightExecute(double tx, double ty, bool tv);
     bool MoveWithLimelightIsFinished(double tx, bool tv);
     void MoveWithLimelightEnd();
