2fb9f3cce16356f0d31d68b23a19b4eee5d8b059
Naina <ngupta.22@presentationhs.org>
2022-02-22 22:36:03 -0800

    Cleaned up/organized button assignments in RobotContainer

--- Comp2022/src/main/cpp/RobotContainer.cpp
+++ Comp2022/src/main/cpp/RobotContainer.cpp
@@ -162,23 +162,20 @@ void RobotContainer::ConfigureButtonBindings()
     // Driver - A, B, X, Y
     m_quickturn.WhileHeld(DriveQuickturn(), true);
 
-    // Driver Bumpers
+    // Driver - Bumpers
     m_intakingDr.WhenPressed(IntakingAction(&m_intake, &m_floorConv, &m_vertConv), true);
     m_intakingDr.WhenReleased(IntakingStop(&m_intake, &m_floorConv, &m_vertConv), true);
-
     m_shootingDr.WhenPressed(ScoringAction(10_s, &m_intake, &m_floorConv, &m_vertConv, &m_shooter), true);
     m_shootingDr.WhenReleased(ScoringStop(&m_intake, &m_floorConv, &m_vertConv, &m_shooter), true);
 
-    // Triggers EXAMPLE ONLY: FIXME WITH CORRECT COMMANDS!
+    // Driver - Triggers
     m_leftTriggerDr.WhenPressed(ShooterAim(true), true);
     m_leftTriggerDr.WhenReleased(ShooterAim(false), true);
-
-    // Driver Trigger for Limelight Mode
     m_rightTriggerDr.WhileHeld(
         DriveLimelightShoot(&m_drivetrain, &m_intake, &m_floorConv, &m_vertConv, &m_shooter, &m_vision));
     m_rightTriggerDr.WhenReleased(ScoringStop(&m_intake, &m_floorConv, &m_vertConv, &m_shooter), true);
 
-    // Start/back
+    // Driver - Start/back
     m_shooterAimOnDr.WhenPressed(ShooterAim(true), true);
     m_shooterAimOffDr.WhenPressed(ShooterAim(false), true);
 
@@ -187,17 +184,16 @@ void RobotContainer::ConfigureButtonBindings()
     frc2::JoystickButton m_exhaustingOp{ &m_operatorController, (int)frc::XboxController::Button::kB };
     frc2::JoystickButton m_scoringOffOp{ &m_operatorController, (int)frc::XboxController::Button::kX };
     frc2::JoystickButton m_inDeployOp{ &m_operatorController, (int)frc::XboxController::Button::kY };
+    frc2::JoystickButton m_climber5RotateInL3Op{ &m_operatorController, (int)frc::XboxController::Button::kStart };
+    frc2::JoystickButton m_intakingOp{ &m_operatorController, (int)frc::XboxController::Button::kLeftBumper };
+    frc2::JoystickButton m_scoringPrimeOp{ &m_operatorController, (int)frc::XboxController::Button::kRightBumper };
+    // frc2::JoystickButton m_shooterAimOnOp{ &m_operatorController, (int)frc::XboxController::Button::kStart };
+    frc2::JoystickButton m_shooterAimOffOp{ &m_operatorController, (int)frc::XboxController::Button::kBack };
 
     frc2::POVButton m_climber0StowOp{ &m_operatorController, 180, 0 };
     frc2::POVButton m_climber1DeployOp{ &m_operatorController, 0, 0 };
     frc2::POVButton m_climber3RotateL3Op{ &m_operatorController, 270, 0 };
     frc2::POVButton m_climber4ExtendL3Op{ &m_operatorController, 90, 0 };
-    frc2::JoystickButton m_climber5RotateInL3Op{ &m_operatorController, (int)frc::XboxController::Button::kStart };
-
-    frc2::JoystickButton m_intakingOp{ &m_operatorController, (int)frc::XboxController::Button::kLeftBumper };
-    frc2::JoystickButton m_scoringPrimeOp{ &m_operatorController, (int)frc::XboxController::Button::kRightBumper };
-    // frc2::JoystickButton m_shooterAimOnOp{ &m_operatorController, (int)frc::XboxController::Button::kStart };
-    frc2::JoystickButton m_shooterAimOffOp{ &m_operatorController, (int)frc::XboxController::Button::kBack };
 
     // Operator - A, B, X, Y
     m_inDeployOp.WhenPressed(IntakeDeploy(true), true);
@@ -205,20 +201,20 @@ void RobotContainer::ConfigureButtonBindings()
     m_exhaustingOp.WhenPressed(ExhaustingAction(&m_intake, &m_floorConv, &m_vertConv), true);
     m_exhaustingOp.WhenReleased(ExhaustingStop(&m_intake, &m_floorConv, &m_vertConv), true);
 
+    // Operator - POV buttons
     m_climber0StowOp.WhenPressed(ClimberStow(&m_climber, &m_intake, &m_floorConv, &m_vertConv, &m_shooter), true);
     m_climber1DeployOp.WhenPressed(ClimberDeploy(&m_climber), true);
     m_climber3RotateL3Op.WhenPressed(ClimberRotateToL3(&m_climber), true);
     m_climber4ExtendL3Op.WhenPressed(ClimberExtendToL3(&m_climber), true);
     m_climber5RotateInL3Op.WhenPressed(ClimberRotateIntoL3(&m_climber), true);
 
-    // Operator Bumpers
+    // Operator - Bumpers
     m_intakingOp.WhenPressed(IntakingAction(&m_intake, &m_floorConv, &m_vertConv), true);
     m_intakingOp.WhenReleased(IntakingStop(&m_intake, &m_floorConv, &m_vertConv), true);
-
     m_scoringPrimeOp.WhenPressed(ScoringPrime(&m_shooter), true);
     m_scoringOffOp.WhenPressed(ScoringStop(&m_intake, &m_floorConv, &m_vertConv, &m_shooter), true);
 
-    // Start/back
+    // Operator - Start/back
     // m_shooterAimOnOp.WhenPressed(ShooterAim(true), true);
     m_shooterAimOffOp.WhenPressed(ShooterAim(false), true);
 }
