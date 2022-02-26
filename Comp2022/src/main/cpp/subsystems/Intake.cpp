f428558a3f17699e88b5f71fb7641ad5faf272c8
amull23 <76454321+amull23@users.noreply.github.com>
2022-02-26 14:10:01 -0800

    Update Intake.cpp

--- Comp2022/src/main/cpp/subsystems/Intake.cpp
+++ Comp2022/src/main/cpp/subsystems/Intake.cpp
@@ -55,9 +55,6 @@ Intake::Intake()
         m_motorIN6.SetNeutralMode(NeutralMode::Coast);
         m_motorIN6.SetSafetyEnabled(false);
 
-        //m_motorIN6.ConfigVoltageCompSaturation(12.0, 0);
-        //m_motorIN6.EnableVoltageCompensation(true);
-
         SupplyCurrentLimitConfiguration supplyCurrentLimits;
         supplyCurrentLimits = { true, 45.0, 45.0, 0.001 };
         m_motorIN6.ConfigSupplyCurrentLimit(supplyCurrentLimits);
