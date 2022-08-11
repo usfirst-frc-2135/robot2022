2327d5ec014b2df3622ac4299f2038f6ffcc32c9
Hannah <hpark.24@presentationhs.org>
2022-08-11 11:35:13 -0700

    Porting Intake to Java and Adding to Constants

--- Offseason2022/src/main/java/frc/robot/subsystems/Intake.java
+++ Offseason2022/src/main/java/frc/robot/subsystems/Intake.java
@@ -4,27 +4,105 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.Intake.Mode;
+import frc.robot.frc2135.RobotConfig;
 
 /**
  *
  */
 public class Intake extends SubsystemBase
 {
-  private WPI_TalonFX motorIN8;
-  private Solenoid    arm;
+  private WPI_TalonFX motorIN8      = new WPI_TalonFX(8);
+  private Solenoid    arm           = new Solenoid(0, PneumaticsModuleType.CTREPCM, 0);
+  private boolean     talonValidIN8 = false;
+  private double      acquireSpeed;
+  private double      expelSpeed;
 
   /**
    *
    */
   public Intake( )
   {
-    motorIN8 = new WPI_TalonFX(6);
-
-    arm = new Solenoid(0, PneumaticsModuleType.CTREPCM, 0);
+    setName("Intake");
+    setSubsystem("Intake");
     addChild("Arm", arm);
+
+    SmartDashboard.putBoolean("HL_IN8Valid", talonValidIN8);
+
+    // Check if solenoids are functional or blacklisted
+    if (arm.isDisabled( ))
+      DataLogManager.log(getSubsystem( ) + "Deploy Solenoid is BLACKLISTED");
+    else
+      DataLogManager.log(getSubsystem( ) + "Deploy Solenoid is FUNCTIONAL");
+
+    RobotConfig config = RobotConfig.getInstance( );
+    acquireSpeed = config.getValueAsDouble("IN_AcquireSpeed", 0.6);
+    expelSpeed = config.getValueAsDouble("IN_ExpelSpeed", -0.6);
+
+    motorIN8.setInverted(true);
+    motorIN8.setSafetyEnabled(false);
+
+    motorIN8.set(0.0);
+
+    initialize( );
+
+  }
+
+  public void initialize( )
+  {
+    DataLogManager.log(getSubsystem( ) + ": subsystem initialized!");
+    setIntakeSpeed(Mode.INTAKE_STOP);
+    setDeployerSolenoid(false);
+  }
+
+  public void setIntakeSpeed(Mode mode)
+  {
+    final String strName;
+    double output = 0.0; // Default: off
+
+    switch (mode)
+    {
+      default :
+      case INTAKE_STOP :
+        strName = "STOP";
+        output = 0.0;
+        break;
+      case INTAKE_ACQUIRE :
+        strName = "ACQUIRE";
+        output = acquireSpeed;
+        break;
+      case INTAKE_EXPEL :
+        strName = "EXPEL";
+        output = expelSpeed;
+        break;
+    }
+
+    // Set speed of intake and the percent output
+    DataLogManager.log(getSubsystem( ) + ": IN Set Speed - {}" + strName);
+
+    motorIN8.set(output);
+  }
+
+  public void setDeployerSolenoid(boolean extended)
+  {
+    if (extended = true)
+    {
+      DataLogManager.log(getSubsystem( ) + "IN Intake {} DEPLOY");
+    }
+    else
+    {
+      DataLogManager.log(getSubsystem( ) + "IN Intake {} STOW");
+    }
+    // DataLogManager.log("IN Intake {}", (extended) ? "DEPLOY" : "STOW");
+    SmartDashboard.putBoolean("IN_Deployed", extended);
+
+    arm.set(extended);
   }
 
   @Override
