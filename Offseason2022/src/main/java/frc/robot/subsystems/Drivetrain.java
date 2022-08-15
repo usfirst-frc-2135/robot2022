76060103ffc23f459d342327228ec7a5222bc8b6
Reeva Randeri <rranderi.24@presentationhs.org>
2022-08-14 19:33:15 -0700

    Added periodic() method

--- Offseason2022/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ Offseason2022/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -169,6 +169,12 @@ public class Drivetrain extends SubsystemBase
   }
 
   @Override
+  public void periodic( )
+  {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
   public void simulationPeriodic( )
   {
     // This method will be called once per scheduler run when in simulation
