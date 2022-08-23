e7eb9612f64b4eb0f1cfe51cf19031482dab08b3
Jeff Mullins <jeff.l.mullins@gmail.com>
2022-08-22 20:11:45 -0700

    Reorder subsystems in robotContainer for a more logical flow

--- Offseason2022/src/main/java/frc/robot/RobotContainer.java
+++ Offseason2022/src/main/java/frc/robot/RobotContainer.java
@@ -77,11 +77,10 @@ import frc.robot.subsystems.TowerConveyor;
 import frc.robot.subsystems.Vision;
 
 /**
- * This class is where the bulk of the robot should be declared. Since
- * Command-based is a "declarative" paradigm, very little robot logic should
- * actually be handled in the {@link Robot} periodic methods (other than the
- * scheduler calls). Instead, the structure of the robot (including subsystems,
- * commands, and button mappings) should be declared here.
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer
 {
@@ -89,16 +88,16 @@ public class RobotContainer
   private static RobotConfig    m_robotConfig    = RobotConfig.getInstance( );
 
   // The robot's subsystems
+  public final Drivetrain       m_drivetrain     = new Drivetrain( );
+  public final Intake           m_intake         = new Intake( );
+  public final FloorConveyor    m_floorConveyor  = new FloorConveyor( );
+  public final TowerConveyor    m_towerConveyor  = new TowerConveyor( );
+  public final Shooter          m_shooter        = new Shooter( );
+  public final Climber          m_climber        = new Climber( );
   public final Vision           m_vision         = new Vision( );
   public final LED              m_led            = new LED( );
-  public final Power            m_power          = new Power( );
   public final Pneumatics       m_pneumatics     = new Pneumatics( );
-  public final Climber          m_climber        = new Climber( );
-  public final Shooter          m_shooter        = new Shooter( );
-  public final TowerConveyor    m_towerConveyor  = new TowerConveyor( );
-  public final FloorConveyor    m_floorConveyor  = new FloorConveyor( );
-  public final Intake           m_intake         = new Intake( );
-  public final Drivetrain       m_drivetrain     = new Drivetrain( );
+  public final Power            m_power          = new Power( );
 
   // Joysticks
   private final XboxController  operator         = new XboxController(1);
@@ -108,8 +107,7 @@ public class RobotContainer
   SendableChooser<Command>      m_chooser        = new SendableChooser<>( );
 
   /**
-   * The container for the robot. Contains subsystems, OI devices, and
-   * commands.
+   * The container for the robot. Contains subsystems, OI devices, and commands.
    */
   private RobotContainer( )
   {
@@ -190,10 +188,9 @@ public class RobotContainer
   }
 
   /**
-   * Use this method to define your button->command mappings. Buttons can be
-   * created by instantiating a {@link GenericHID} or one of its subclasses
-   * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and
-   * then passing it to a
+   * Use this method to define your button->command mappings. Buttons can be created by instantiating
+   * a {@link GenericHID} or one of its subclasses ({@link edu.wpi.first.wpilibj.Joystick} or
+   * {@link XboxController}), and then passing it to a
    * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings( )
