af5383f505cf4abcf6a3792b9bd6f79bd24c97cd
Hannah <hpark.24@presentationhs.org>
2022-08-25 16:58:01 -0700

    Porting AutoWait to Java

--- Offseason2022/src/main/java/frc/robot/commands/AutoWait.java
+++ Offseason2022/src/main/java/frc/robot/commands/AutoWait.java
@@ -3,19 +3,24 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.subsystems.Drivetrain;
 
 /**
  *
  */
 public class AutoWait extends WaitCommand
 {
-  private double m_timeout;
+  private int        m_timerNum;
+  private Drivetrain m_drivetrain;
+  private double     m_waitTime;
 
-  public AutoWait(double timeout)
+  public AutoWait(Drivetrain drivetrain, int timerNum)
   {
-    super(timeout);
-    m_timeout = timeout;
+    super(timerNum);
+    m_timerNum = timerNum;
+    m_drivetrain = drivetrain;
 
     // m_subsystem = subsystem;
     // addRequirements(m_subsystem);
@@ -26,18 +31,34 @@ public class AutoWait extends WaitCommand
   public void initialize( )
   {
     super.initialize( );
+    if (m_timerNum == 1)
+      m_waitTime = SmartDashboard.getNumber("AUTO_WaitTime1", 0.0);
+
+    if (m_timerNum == 2)
+      m_waitTime = SmartDashboard.getNumber("AUTO_WaitTime2", 0.0);
+
+    m_timer.reset( );
+    m_timer.start( );
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   public void execute( )
-  {}
+  {
+    m_drivetrain.moveStop( );
+  }
+
+  public boolean IsFinished( )
+  {
+    return m_timer.hasElapsed(m_waitTime);
+  }
 
   // Called once after isFinished returns true
   @Override
   public void end(boolean interrupted)
   {
     super.end(interrupted);
+    m_timer.stop( );
   }
 
   @Override
