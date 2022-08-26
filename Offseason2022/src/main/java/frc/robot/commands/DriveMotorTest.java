1ffd76f6148e1eb5654058e6339e14de8ab9ca0f
Hannah <hpark.24@presentationhs.org>
2022-08-25 17:45:52 -0700

    Porting DriveMotorTest to Java + Updating Robot Container for it

--- Offseason2022/src/main/java/frc/robot/commands/DriveMotorTest.java
+++ Offseason2022/src/main/java/frc/robot/commands/DriveMotorTest.java
@@ -4,16 +4,23 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.Drivetrain;
 
 /**
  *
  */
 public class DriveMotorTest extends CommandBase
 {
-  public DriveMotorTest( )
+  private Drivetrain m_drivetrain;
+  private boolean    m_left;
+
+  public DriveMotorTest(boolean left, Drivetrain drivetrain)
   {
-    // m_subsystem = subsystem;
+    m_drivetrain = drivetrain;
     // addRequirements(m_subsystem);
+    setName("DriveMotorTest");
+    addRequirements(m_drivetrain);
   }
 
   // Called when the command is initially scheduled.
@@ -24,7 +31,13 @@ public class DriveMotorTest extends CommandBase
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute( )
-  {}
+  {
+    RobotContainer robotContainer = RobotContainer.getInstance( );
+    if (m_left)
+      robotContainer.m_drivetrain.TankDriveVolts(3.0, 0.0);
+    else
+      robotContainer.m_drivetrain.TankDriveVolts(0.0, 3.0);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
