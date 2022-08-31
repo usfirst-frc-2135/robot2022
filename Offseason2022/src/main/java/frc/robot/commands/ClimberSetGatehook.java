d02df086de72210a56a31341ee913e80a383f48d
Hannah <hpark.24@presentationhs.org>
2022-08-30 20:03:32 -0700

    Porting Climber Calibrate, Run, SetGatehook, TimerOverride to Java

--- Offseason2022/src/main/java/frc/robot/commands/ClimberSetGatehook.java
+++ Offseason2022/src/main/java/frc/robot/commands/ClimberSetGatehook.java
@@ -4,22 +4,28 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
 
 /**
  *
  */
 public class ClimberSetGatehook extends CommandBase
 {
-  public ClimberSetGatehook( )
+  private boolean m_climberGateHookClosed;
+
+  public ClimberSetGatehook(boolean climberSetGateHook)
   {
-    // m_subsystem = subsystem;
-    // addRequirements(m_subsystem);
+    m_climberGateHookClosed = climberSetGateHook;
+    setName("ClimberSetGateHook");
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize( )
-  {}
+  {
+    RobotContainer robotContainer = RobotContainer.getInstance( );
+    robotContainer.m_climber.setGateHook(m_climberGateHookClosed);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
@@ -35,7 +41,7 @@ public class ClimberSetGatehook extends CommandBase
   @Override
   public boolean isFinished( )
   {
-    return false;
+    return true;
   }
 
   @Override
--- Offseason2022/src/main/java/frc/robot/commands/ClimberTimerOverride.java
+++ Offseason2022/src/main/java/frc/robot/commands/ClimberTimerOverride.java
@@ -3,23 +3,31 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.Climber;
 
 /**
  *
  */
 public class ClimberTimerOverride extends CommandBase
 {
-  public ClimberTimerOverride( )
+  private Climber        m_climber;
+  private XboxController operatorController;
+
+  public ClimberTimerOverride(Climber climber)
   {
-    // m_subsystem = subsystem;
-    // addRequirements(m_subsystem);
+    m_climber = climber;
+    setName("ClimberTimerOverride");
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize( )
-  {}
+  {
+    operatorController = RobotContainer.getInstance( ).getOperator( );
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
@@ -35,7 +43,9 @@ public class ClimberTimerOverride extends CommandBase
   @Override
   public boolean isFinished( )
   {
-    return false;
+    // return operatorController->GetRawButtonPressed((int)frc::XboxController::Button::kY);
+
+    return operatorController.getRawButtonPressed(XboxController.Button.kY.value);
   }
 
   @Override
