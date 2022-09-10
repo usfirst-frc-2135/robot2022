27dc2c0c4e33fe88d782ab721f5acee837201341
Hannah <hpark.24@presentationhs.org>
2022-09-10 14:38:25 -0700

    Porting Climber3RotateToL3

--- Offseason2022/src/main/java/frc/robot/commands/Climber3RotateToL3.java
+++ Offseason2022/src/main/java/frc/robot/commands/Climber3RotateToL3.java
@@ -3,7 +3,11 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import frc.robot.Constants.CLConsts.Height;
 import frc.robot.subsystems.Climber;
 
 /**
@@ -13,7 +17,10 @@ public class Climber3RotateToL3 extends SequentialCommandGroup
 {
   public Climber3RotateToL3(Climber climber)
   {
-    addCommands(
+    setName("ClimberRotateToL3");
+
+    addCommands(new ParallelCommandGroup(new ParallelDeadlineGroup(new WaitUntilCommand(climber::moveClimberDistanceIsFinished),
+        new ClimberMoveToHeight(climber, Height.ROTATE_L3_HEIGHT)), new ClimberSetGatehook(climber, true))
     // Add Commands here:
     // Also add parallel commands using the
     //
