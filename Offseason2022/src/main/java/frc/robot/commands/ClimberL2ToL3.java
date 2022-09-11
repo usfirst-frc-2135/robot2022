d21ccf03c8964a1684a9335822b6fd3b142ccdbb
Hannah <hpark.24@presentationhs.org>
2022-09-11 09:54:19 -0700

    Porting ClimberFullClimb + ClimberL2ToL3 + ClimberL3ToL4
    Also updating Constants for the GCs

--- Offseason2022/src/main/java/frc/robot/commands/ClimberL2ToL3.java
+++ Offseason2022/src/main/java/frc/robot/commands/ClimberL2ToL3.java
@@ -4,6 +4,8 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Constants.CLConsts;
 import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.FloorConveyor;
 import frc.robot.subsystems.Intake;
@@ -15,19 +17,29 @@ import frc.robot.subsystems.TowerConveyor;
  */
 public class ClimberL2ToL3 extends SequentialCommandGroup
 {
+  private double m_climbL2Timer         = CLConsts.kClimbL2Timer;
+  private double m_rotateExtendL3Timer  = CLConsts.kRotateExtendL3Timer;
+  private double m_rotateRetractL3Timer = CLConsts.kRotateRetractL3Timer;
+  private double m_climbL3Timer         = CLConsts.kClimbL3Timer;
+
   public ClimberL2ToL3(Climber climber, Intake intake, FloorConveyor fConv, TowerConveyor tConv, Shooter shooter)
   {
+    setName("ClimberL2ToL3");
+
     addCommands(
-    // Add Commands here:
-    // Also add parallel commands using the
-    //
-    // addCommands(
-    // new command1(argsN, subsystem),
-    // parallel(
-    // new command2(argsN, subsystem),
-    // new command3(argsN, subsystem)
-    // )
-    // );
+        // Add Commands here:
+
+        // @formatter:off
+      new Climber2ClimbToL2(climber),
+      new WaitCommand(m_climbL2Timer), // check to see if timer is necessary here
+      new Climber3RotateToL3(climber),
+      new WaitCommand(m_rotateExtendL3Timer),
+      new Climber5RotateIntoL3(climber),
+      new WaitCommand(m_rotateRetractL3Timer),
+      new Climber6ClimbToL3(climber),
+      new WaitCommand(m_climbL3Timer)
+      
+      // @formatter:on
 
     );
   }
