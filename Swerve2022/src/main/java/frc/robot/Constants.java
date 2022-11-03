04c1a85f3b40941a9c0ea018a27685416608767b
Reeva Randeri <rranderi.24@presentationhs.org>
2022-11-03 15:44:51 -0700

    Added in subsystems and PhoenixUtil files

--- Swerve2022/src/main/java/frc/robot/Constants.java
+++ Swerve2022/src/main/java/frc/robot/Constants.java
@@ -1,6 +1,8 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.sensors.SensorVelocityMeasPeriod;
+
 import edu.wpi.first.math.util.Units;
 
 /**
@@ -51,9 +53,170 @@ public class Constants
     public static final int    kLRCANCoderCANID       = 3;
     public static final int    kRRCANCoderCANID       = 4;
 
+    // Drivetrain specs
     public static final double kWheelDiaMeters        = Units.inchesToMeters(4.0); // 4in (39.37 in/meter)
     public static final double kGearRatio             = 6.75;
     public static final double kEncoderMetersPerCount = (kWheelDiaMeters * Math.PI) / (Falcon500.kEncoderCPR) / kGearRatio;
+    public static final double kTrackWidthMeters      = 0.6477;        // Measured track width
 
+    // Joystick tuning
+    public static final double kDriveXScaling         = 1.0;           // 1.0 is no scaling
+    public static final double kDriveYScaling         = 1.0;           // 1.0 is no scaling
+    public static final double kQuickTurnScaling      = 0.5;           // Scale by 50% of full speed
+    public static final double kSlowClimbScaling      = 0.3;           // Scale by 30% of full speed
+
+    // Measured characterization
+    public static final double ks                     = 0.65;          // Volts
+    public static final double kv                     = 2.84;          // Volts / mps
+    public static final double ka                     = 0.309;         // Volts / (mps^2)
+    public static final double KvAngular              = 1.5;           // Volts / (rad/sec)
+    public static final double KaAngular              = 0.3;           // Volts / (rad/sec^2)
+
+    // Teleop driving controls
+    public static final double kOpenLoopRamp          = 0.5;           // CTRE: full speed in 0.5 sec
+    public static final double kClosedLoopRamp        = 0.0;           // CTRE: 0 is disabled
+    public static final double kStopTolerance         = 0.05;          // Target position tolerance (< 5cm)
   }
+
+  public static final class INConsts
+  {
+    // public static final int kIN8CANID = 6;
+    public static final int    kINPWM1         = 1;
+    public static final int    kArmSolenoid    = 0;
+
+    public static final double kINAcquireSpeed = 0.6;
+    public static final double kINExpelSpeed   = -0.6;
+
+    public enum INMode
+    {
+      INTAKE_STOP,    // Stop intake motor
+      INTAKE_ACQUIRE, // Acquire game pieces
+      INTAKE_EXPEL,   // Expel game pieces
+    }
+  }
+
+  public static final class FCConsts
+  {
+    public static final int    kFC8CANID           = 8;
+
+    public static final double kFCAcquireSpeed     = 1.0;
+    public static final double kFCAcquireSpeedSlow = 0.2;
+    public static final double kFCExpelSpeedFast   = -1.0;
+
+    public enum FCMode
+    {
+      FCONVEYOR_STOP,       // Stop floor conveyor motor
+      FCONVEYOR_ACQUIRE,    // Aquire game pieces
+      FCONVEYOR_EXPEL,      // Expel game pieces
+      FCONVEYOR_EXPEL_FAST, // Expel Fast
+    }
+  }
+
+  public static final class TCConsts
+  {
+    public static final int    kTC9CANID           = 9;
+    public static final int    kCargoDIO           = 2;
+
+    public static final double kTCAcquireSpeed     = 1.0;
+    public static final double kTCAcquireSpeedSlow = 0.2;
+    public static final double kTCExpelSpeed       = -0.2;
+    public static final double kTCExpelSpeedFast   = -1.0;
+
+    public enum TCMode
+    {
+      TCONVEYOR_STOP,         // Conveyor stop
+      TCONVEYOR_ACQUIRE,      // Conveyor moves game pieces to shooter
+      TCONVEYOR_ACQUIRE_SLOW, // Conveyor moves during game piece intake
+      TCONVEYOR_EXPEL,        // Conveyor moves game pieces to hopper
+      TCONVEYOR_EXPEL_FAST,   // Conveyor moves game pieces to hopper
+    }
+  }
+
+  public static final class SHConsts
+  {
+    public static final int                      kSH11CANID               = 11;
+
+    public static final double                   kFlywheelGearRatio       = (18.0 / 12.0);
+    public static final double                   kFlywheelCPR             = Falcon500.kEncoderCPR * kFlywheelGearRatio;
+
+    public static final int                      kVelocityMeasWindow      = 1;
+    public static final SensorVelocityMeasPeriod kVelocityMeasPeriod      = SensorVelocityMeasPeriod.Period_10Ms;
+    public static final double                   kFlywheelPidKf           = 0.04775;
+    public static final double                   kFlywheelPidKp           = 0.2;
+    public static final double                   kFlywheelPidKi           = 0.0;
+    public static final double                   kFlywheelPidKd           = 0.0;
+    public static final double                   kFlywheelNeutralDeadband = 0.01;
+
+    public static final double                   kFlywheelToleranceRPM    = 150.0;     // Tolerance band around target RPM
+    public static final double                   kFlywheelLowerTargetRPM  = 1000.0;    // RPM for lower hub
+    public static final double                   kFlywheelUpperTargetRPM  = 2150.0;    // RPM for upper hub
+    public static final double                   kFlywheelPrimeRPM        = kFlywheelUpperTargetRPM; // RPM for shooter priming
+
+    public static final double                   kReverseRPMThreshold     = 20.0;      // RPM threshold for allowing reverse
+    public static final double                   kFlywheelReverseRPM      = -1000.0;   // RPM for reversing out game pieces
+
+    public enum SHMode
+    {
+      SHOOTER_REVERSE,    // Shooter runs in reverse direction to handle jams
+      SHOOTER_STOP,       // Shooter is stopped
+      SHOOTER_PRIME,      // Shooter ramped to an initial speed before shooting
+      SHOOTER_LOWERHUB,   // Shooter at speed for low hub
+      SHOOTER_UPPERHUB,   // Shooter at speed for high hub
+    }
+  }
+
+  public static final class VIConsts
+  {
+    // Limelight-defined streaming states
+    public static final int STANDARD      = 0;  // Both cameras side-by-side
+    public static final int PIP_MAIN      = 1;  // Limelight with second camera inset
+    public static final int PIP_SECONDARY = 2;  // Second camera with limelight inset
+
+    // Limelight-defined LED mode states
+    public static final int LED_OFF       = 1;
+    public static final int LED_ON        = 3;
+
+    public enum VIRequests
+    {
+      VISION_OFF, VISION_ON, VISION_TOGGLE
+    }
+
+    public static final double kLLDistance1   = 48;    // distance from bumper in inches for first reference point
+    public static final double kLLVertOffset1 = 0.42;  // LL y reading in degrees for first reference point
+    public static final double kLLDistance2   = 60;    // distance from bumper in inches for second reference point
+    public static final double kLLVertOffset2 = -4.85; // LL y reading in degrees for second reference point
+  }
+
+  public static final class LEDConsts
+  {
+    public static final int kCANDdleID = 0;
+
+    public enum LEDColor
+    {
+      LEDCOLOR_OFF,     // CANdle off
+      LEDCOLOR_WHITE,   // CANdle white
+      LEDCOLOR_RED,     // CANdle red
+      LEDCOLOR_ORANGE,  // CANdle orange
+      LEDCOLOR_YELLOW,  // CANdle yellow
+      LEDCOLOR_GREEN,   // CANdle green
+      LEDCOLOR_BLUE,    // CANdle blue
+      LEDCOLOR_PURPLE,  // CANdle purple
+      LEDCOLOR_DASH     // CANdle color taken from dashboard
+    }
+  }
+
+  public static final class SIMLLConsts
+  {
+    public static final double kFieldLength        = Units.feetToMeters(54.0);      // Field dimensions are 54ft x 27ft
+    public static final double kFieldWidth         = Units.feetToMeters(27.0);
+    public static final double kGoalPostionX       = kFieldLength / 2 - Units.feetToMeters(2.0); // Goal target on field
+    public static final double kGoalPostionY       = kFieldWidth / 2;
+    public static final double kGoalHeight         = Units.inchesToMeters(102.81);  // Upper hub height from floor
+    public static final double kCameraPositionX    = Units.inchesToMeters(0.0);     // Camera position on robot (X, Y)
+    public static final double kCameraPositionY    = Units.inchesToMeters(0.0);
+    public static final double kCameraRotation     = Units.degreesToRadians(180.0); // Camera rotation on robot
+    public static final double kCameraLensHeight   = Units.inchesToMeters(41.0);    // Camera lens height from floor
+    public static final double kCameraLensBackTilt = Units.degreesToRadians(40.0);  // Camera backward tilt from normal
+  }
+
 }
--- Swerve2022/src/main/java/frc/robot/subsystems/Pneumatics.java
+++ Swerve2022/src/main/java/frc/robot/subsystems/Pneumatics.java
@@ -4,31 +4,38 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.PneumaticsControlModule;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.commands.*;
 
 /**
  *
  */
 public class Pneumatics extends SubsystemBase
 {
-  private Compressor compressor;
+  private final PneumaticsControlModule m_pcm        = new PneumaticsControlModule(0);
+  private final Compressor              m_compressor = new Compressor(PneumaticsModuleType.CTREPCM);
 
   /**
-  *
-  */
+   *
+   */
   public Pneumatics( )
   {
-    compressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
-    addChild("Compressor", compressor);
+    setName("Pneumatics");
+    setSubsystem("Pneumatics");
+    addChild("Compressor", m_compressor);
+
+    initialize( );
   }
 
   @Override
   public void periodic( )
   {
     // This method will be called once per scheduler run
+
+    SmartDashboard.putNumber("PCM_Output_Comp", m_pcm.getCompressorCurrent( ));
   }
 
   @Override
@@ -38,4 +45,26 @@ public class Pneumatics extends SubsystemBase
   }
 
   // Put methods for controlling this subsystem here. Call these from Commands.
+
+  public void initialize( )
+  {
+    DataLogManager.log(getSubsystem( ) + ": subsystem initialized!");
+  }
+
+  public void faultDump( )
+  {
+    // Print out PCM faults and clear sticky ones
+    DataLogManager.log(getSubsystem( ) + ": ----- PCM FAULTS --------------");
+
+    if (m_pcm.getCompressorCurrentTooHighFault( ))
+      DataLogManager.log(getSubsystem( ) + ": Warn - CurrentTooHighFault");
+    if (m_pcm.getCompressorNotConnectedFault( ))
+      DataLogManager.log(getSubsystem( ) + ": Warn - CompressorNotConnectedFault");
+    if (m_pcm.getCompressorShortedFault( ))
+      DataLogManager.log(getSubsystem( ) + ": Warn - CompressorShortedFault");
+    if (m_pcm.getSolenoidVoltageFault( ))
+      DataLogManager.log(getSubsystem( ) + ": Warn - SolenoidVoltageFault");
+
+    m_pcm.clearAllStickyFaults( );
+  }
 }
