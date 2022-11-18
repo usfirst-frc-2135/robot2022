c9613fb94ac8e579a816f442d591b034d827464a
Jeff Mullins <jeff.l.mullins@gmail.com>
2022-11-17 22:27:46 -0800

    Remove more 1678 and 254 files that aren't needed

--- Swerve2022/src/main/java/frc/robot/team1678/frc2022/drivers/Pigeon.java
+++ Swerve2022/src/main/java/frc/robot/team1678/frc2022/drivers/Pigeon.java
@@ -1,73 +1,74 @@
 package frc.robot.team1678.frc2022.drivers;
 
 import com.ctre.phoenix.sensors.Pigeon2;
+
 import frc.robot.team1678.frc2022.Constants;
 import frc.robot.team1678.frc2022.Ports;
 import frc.robot.team254.lib.geometry.Rotation2d;
 
 public class Pigeon {
 
-    private static Pigeon mInstance;
+  private static Pigeon mInstance;
 
-    public static Pigeon getInstance() {
-        if (mInstance == null) {
-            mInstance = new Pigeon(Ports.PIGEON);
-        }
-        return mInstance;
+  public static Pigeon getInstance() {
+    if (mInstance == null) {
+      mInstance = new Pigeon(Ports.PIGEON);
     }
+    return mInstance;
+  }
 
-    // Actual pigeon object
-    private final Pigeon2 mGyro;
+  // Actual pigeon object
+  private final Pigeon2 mGyro;
 
-    // Configs
-    private boolean inverted = Constants.SwerveConstants.invertGyro;
-    private Rotation2d yawAdjustmentAngle = Rotation2d.identity();
-    private Rotation2d rollAdjustmentAngle = Rotation2d.identity();
+  // Configs
+  private boolean inverted = Constants.SwerveConstants.invertGyro;
+  private Rotation2d yawAdjustmentAngle = Rotation2d.identity();
+  private Rotation2d rollAdjustmentAngle = Rotation2d.identity();
 
-    private Pigeon(int port) {        
-        mGyro = new Pigeon2(port, "canivore1");
-        mGyro.configFactoryDefault();
+  private Pigeon(int port) {
+    mGyro = new Pigeon2(port, "canivore1");
+    mGyro.configFactoryDefault();
+  }
+
+  public Rotation2d getYaw() {
+    Rotation2d angle = getUnadjustedYaw().rotateBy(yawAdjustmentAngle.inverse());
+    if (inverted) {
+      return angle.inverse();
     }
+    return angle;
+  }
 
-    public Rotation2d getYaw() {
-        Rotation2d angle = getUnadjustedYaw().rotateBy(yawAdjustmentAngle.inverse());
-        if (inverted) {
-            return angle.inverse();
-        }
-        return angle;
-    }
+  public Rotation2d getRoll() {
+    return getUnadjustedRoll().rotateBy(rollAdjustmentAngle.inverse());
+  }
 
-    public Rotation2d getRoll() {
-        return getUnadjustedRoll().rotateBy(rollAdjustmentAngle.inverse());
-    }
+  /**
+   * Sets the yaw register to read the specified value.
+   *
+   * @param angleDeg New yaw in degrees
+   */
+  public void setYaw(double angleDeg) {
+    yawAdjustmentAngle = getUnadjustedYaw().rotateBy(Rotation2d.fromDegrees(angleDeg).inverse());
+  }
 
-    /**
-     * Sets the yaw register to read the specified value.
-     *
-     * @param angleDeg New yaw in degrees
-     */
-    public void setYaw(double angleDeg) {
-        yawAdjustmentAngle = getUnadjustedYaw().rotateBy(Rotation2d.fromDegrees(angleDeg).inverse());
-    }
+  /**
+   * Sets the roll register to read the specified value.
+   *
+   * @param angleDeg New yaw in degrees
+   */
+  public void setRoll(double angleDeg) {
+    rollAdjustmentAngle = getUnadjustedRoll().rotateBy(Rotation2d.fromDegrees(angleDeg).inverse());
+  }
 
-    /**
-     * Sets the roll register to read the specified value.
-     *
-     * @param angleDeg New yaw in degrees
-     */
-    public void setRoll(double angleDeg) {
-        rollAdjustmentAngle = getUnadjustedRoll().rotateBy(Rotation2d.fromDegrees(angleDeg).inverse());
-    }
+  public Rotation2d getUnadjustedYaw() {
+    return Rotation2d.fromDegrees(mGyro.getYaw());
+  }
 
-    public Rotation2d getUnadjustedYaw() {
-        return Rotation2d.fromDegrees(mGyro.getYaw());
-    }
+  public Rotation2d getUnadjustedPitch() {
+    return Rotation2d.fromDegrees(mGyro.getPitch());
+  }
 
-    public Rotation2d getUnadjustedPitch() {
-        return Rotation2d.fromDegrees(mGyro.getPitch());
-    }
-
-    public Rotation2d getUnadjustedRoll() {
-        return Rotation2d.fromDegrees(mGyro.getRoll());
-    }
+  public Rotation2d getUnadjustedRoll() {
+    return Rotation2d.fromDegrees(mGyro.getRoll());
+  }
 }
--- Swerve2022/src/main/java/frc/robot/team1678/frc2022/loops/ILooper.java
+++
@@ -1,5 +0,0 @@
-package frc.robot.team1678.frc2022.loops;
-
-public interface ILooper {
-    void register(Loop loop);
-}
